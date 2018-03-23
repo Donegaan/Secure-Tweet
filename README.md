9321ef3753459552cd2af2fb7ad1834db04effd8
donegaan <donegaan@tcd.ie>
2018-03-23 12:26:57 +0000

    Enter value works

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CS3031-Secure-App
 Chrome extension to secure a social media app using a key management system
+
+Encrypt tweets on page, display encrypted
+
+User needs to enter key to decrypt tweets
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-var stuff = $("div.js-tweet-text-container > p").text("Blocked");
+block();
+function block(){
+    console.log("block called");
+    var stuff = $("div.js-tweet-text-container > p").text("Blocked");
+}
+$(function(){
+    $('#keyInput').click(function(){
+        $('#test').text($('#input').val());
+    });
+});
+
+// function getKey(){
+//     console.log("get key called");
+//     alert(document.getElementById('keyInput').values+" is value");
+// }
