f43bba8c1d5e04cf5d16d099b2ac2808932bb2e7
donegaan <donegaan@tcd.ie>
2018-03-23 19:49:44 +0000

    Writes full cipher Json to tweet

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-block();
-function block(){
-    console.log("block called");
-    var stuff = $("div.js-tweet-text-container > p").text("Blocked");
-}
-$(function(){
-    $('#keyInput').click(function(){
+// block();
+
+// function block() {
+//     var stuff = $("div.js-tweet-text-container > p").text("Blocked");
+// }
+$(function () {
+    $('#keyInput').click(function () {
         $('#test').text($('#input').val());
     });
 });
+
+testEncrypt(); 
+function testEncrypt() { // Using sjcl encryption library
+    var password = "password";
+    var text = $("div.js-tweet-text-container > p").text();
+    // var text = "test stuff";
+    // console.log(text);
+    var parameters = {
+        "iter": 1000
+    };
+    var rp = {};
+    var cipherTextJson = {};
+
+    sjcl.misc.cachedPbkdf2(password, parameters);
+    cipherTextJson = sjcl.encrypt(password, text, parameters, rp);
+    console.log(cipherTextJson);
+    $("div.js-tweet-text-container > p").text(""+cipherTextJson);
+
+    var decryptedText = sjcl.decrypt(password, cipherTextJson);
+    console.log(decryptedText);
+}
 
 // function getKey(){
 //     console.log("get key called");
 //     alert(document.getElementById('keyInput').values+" is value");
 // }
+
