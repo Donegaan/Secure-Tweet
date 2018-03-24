1abb07eb8a8c957352906d485c918c56824f690a
donegaan <donegaan@tcd.ie>
2018-03-24 15:13:15 +0000

    Displays all cipher text in tweet box. Need to separate tweets

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ $(function () {
     });
 });
 
-testEncrypt(); 
+testEncrypt();
+
 function testEncrypt() { // Using sjcl encryption library
     var password = "password";
     var text = $("div.js-tweet-text-container > p").text();
@@ -20,18 +21,33 @@ function testEncrypt() { // Using sjcl encryption library
     };
     var rp = {};
     var cipherTextJson = {};
+    
 
     sjcl.misc.cachedPbkdf2(password, parameters);
     cipherTextJson = sjcl.encrypt(password, text, parameters, rp);
-    console.log(cipherTextJson);
-    $("div.js-tweet-text-container > p").text(""+cipherTextJson);
+    var parsed = JSON.parse(cipherTextJson);
+    $("div.js-tweet-text-container > p").text("" + parsed.ct); // Displays cipher text in tweet
 
     var decryptedText = sjcl.decrypt(password, cipherTextJson);
+
     console.log(decryptedText);
+     
+        // Layout of cipherJson
+        // cipherTextJson={
+    //     "iv":"tjp81jkAzUpW1bI9gLDDpg==", // iv Base64 encoded
+    //     "v":1,                           // version
+    //     "iter":1000,                     // iteration count
+    //     "ks":128,                        // key size in bits
+    //     "ts":64,                         // authentication strength
+    //     "mode":"ccm",                    // mode
+    //     "adata":"xxx",                   // authenticated data
+    //     "cipher":"aes",                  // cipher
+    //     "salt":"lx06UoJDNys=",           // key derivation salt
+    //     "ct":"Gv7ptKdTtUz6AGtX"          // ciphet text
+    //     };
 }
 
 // function getKey(){
 //     console.log("get key called");
 //     alert(document.getElementById('keyInput').values+" is value");
 // }
-
