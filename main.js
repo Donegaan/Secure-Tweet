e94559b8ceba4c6491bd7859b1029913eba54301
donegaan <donegaan@tcd.ie>
2018-03-26 17:14:55 +0100

    Changes tweet on correct key entry. Get decrupt working

--- main.js
+++ main.js
@@ -1,53 +1,42 @@
-// block();
 
-// function block() {
-//     var stuff = $("div.js-tweet-text-container > p").text("Blocked");
-// }
-$(function () {
+//var stuff = $("div.js-tweet-text-container <p class=\"TweetTextSize TweetTextSize--normal js-tweet-text tweet-text\" lang=\"en\" data-aria-label-part=\"4\">").text("Blocked");
+
+var cipherTextJson = {};
+// var password = "password";
+group ={ // Key Management with JSON object
+    "key": "password",
+    "emails":{
+
+    }
+};
+
+$(function () { // Take in input
     $('#keyInput').click(function () {
-        $('#test').text($('#input').val());
+        $('#test').text($('#input').val()); // Take input string
+        if(group.key===$('#input').val()){ // Display decrypted tweet if key is correct
+            // console.log("CALL");
+            var decryptedText = sjcl.decrypt(group.key, cipherTextJson);
+            chrome.tabs.executeScript({
+                code: '$("div.js-tweet-text-container > p").text("'+decryptedText+'")'
+            });
+        }else{
+            alert("Incorrect decryption key entered");
+        }
     });
 });
 
-testEncrypt();
+encrypt();
 
-function testEncrypt() { // Using sjcl encryption library
-    var password = "password";
+function encrypt(){
     var text = $("div.js-tweet-text-container > p").text();
-    // var text = "test stuff";
-    // console.log(text);
     var parameters = {
         "iter": 1000
     };
     var rp = {};
-    var cipherTextJson = {};
-    
-
-    sjcl.misc.cachedPbkdf2(password, parameters);
-    cipherTextJson = sjcl.encrypt(password, text, parameters, rp);
+    sjcl.misc.cachedPbkdf2(group.key, parameters);
+    cipherTextJson = sjcl.encrypt(group.key, text, parameters, rp);
     var parsed = JSON.parse(cipherTextJson);
     $("div.js-tweet-text-container > p").text("" + parsed.ct); // Displays cipher text in tweet
-
-    var decryptedText = sjcl.decrypt(password, cipherTextJson);
-
-    console.log(decryptedText);
-     
-        // Layout of cipherJson
-        // cipherTextJson={
-    //     "iv":"tjp81jkAzUpW1bI9gLDDpg==", // iv Base64 encoded
-    //     "v":1,                           // version
-    //     "iter":1000,                     // iteration count
-    //     "ks":128,                        // key size in bits
-    //     "ts":64,                         // authentication strength
-    //     "mode":"ccm",                    // mode
-    //     "adata":"xxx",                   // authenticated data
-    //     "cipher":"aes",                  // cipher
-    //     "salt":"lx06UoJDNys=",           // key derivation salt
-    //     "ct":"Gv7ptKdTtUz6AGtX"          // ciphet text
-    //     };
+    
+    // return cipherTextJson;
 }
-
-// function getKey(){
-//     console.log("get key called");
-//     alert(document.getElementById('keyInput').values+" is value");
-// }
