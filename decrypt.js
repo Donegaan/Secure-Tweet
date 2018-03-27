cd3cd3aca36ec8c280bec753efcded999a657d1d
donegaan <donegaan@tcd.ie>
2018-03-27 20:17:04 +0100

    Got individual tweet encrypt and decrypt working

--- decrypt.js
+++ decrypt.js
@@ -1,21 +1,24 @@
-var text = $("div.js-tweet-text-container > p").text();
-var ct = text.substring(0, text.indexOf("҂"));
-var salt = text.substring(text.indexOf("҂")+1,text.indexOf("؏"));
-var iv =text.substring(text.indexOf("؏")+1,text.indexOf("௳"));
-console.log("text: " + text + " ct: " + ct + " salt: " + salt + " iv: " + iv);
-cipherTweet = {
-    "iv": iv,
-    "v": 1,
-    "iter": 1000,
-    "ks": 128,
-    "ts": 64,
-    "mode": "ccm",
-    "adata": "",
-    "cipher": "aes",
-    "salt": salt,
-    "ct": ct
-};
-var cipherJson = JSON.stringify(cipherTweet);
-console.log(cipherJson);
-var decryptedText = sjcl.decrypt(group.key, cipherJson);
-$("div.js-tweet-text-container > p").text(""+decryptedText);
+$("div.js-tweet-text-container > p").each(function () { // To get each individual tweet
+    var text = $(this).text();
+    var ct = text.substring(0, text.indexOf("҂"));
+    var salt = text.substring(text.indexOf("҂") + 1, text.indexOf("؏"));
+    var iv = text.substring(text.indexOf("؏") + 1, text.indexOf("௳"));
+    console.log("text: " + text + " ct: " + ct + " salt: " + salt + " iv: " + iv);
+    cipherTweet = {
+        "iv": iv,
+        "v": 1,
+        "iter": 1000,
+        "ks": 128,
+        "ts": 64,
+        "mode": "ccm",
+        "adata": "",
+        "cipher": "aes",
+        "salt": salt,
+        "ct": ct
+    };
+    var cipherJson = JSON.stringify(cipherTweet);
+    console.log(cipherJson);
+    var decryptedText = sjcl.decrypt(group.key, cipherJson);
+    var displayText = decryptedText.substring(0, decryptedText.indexOf("pic.twitter.com"));
+    $(this).text("" + displayText);
+});
--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-
 //var stuff = $("div.js-tweet-text-container <p class=\"TweetTextSize TweetTextSize--normal js-tweet-text tweet-text\" lang=\"en\" data-aria-label-part=\"4\">").text("Blocked");
 
 // var cipherTextJson = {};
 // var password = "password";
-group ={ // Key Management with JSON object
+group = { // Key Management with JSON object
     "key": "password",
 };
+things={
+    
+};
+var parameters = {
+    "iter": 1000
+};
+var rp = {};
 
 $(function () { // Take in input
     $('#keyInput').click(function () {
-        if(group.key===$('#keyTextInput').val()){ // Display decrypted tweet if key is correct
+        if (group.key === $('#keyTextInput').val()) { // Display decrypted tweet if key is correct
             // console.log("CALL");
-            
+
             chrome.tabs.executeScript({
                 // code: '$("div.js-tweet-text-container > p").text("'+decryptedText+'")'
                 file: 'decrypt.js'
             });
-        }else{
+        } else {
             alert("Incorrect decryption key entered");
         }
     });
@@ -24,18 +30,16 @@ $(function () { // Take in input
 
 encrypt();
 
-function encrypt(){
-    var text = $("div.js-tweet-text-container > p").text(); // .each to get each separate
-    var parameters = {
-        "iter": 1000
-    };
-    var rp = {};
-    sjcl.misc.cachedPbkdf2(group.key, parameters);
-    cipherTextJson = sjcl.encrypt(group.key, text, parameters, rp);
+function encrypt() {
+    $("div.js-tweet-text-container > p").each(function () {
+        // console.log($("div.js-tweet-text-container > p").text()+"\n");
+        var text = $(this).text(); // .each to get each separate
 
-    var parsed = JSON.parse(cipherTextJson);
-    console.log(cipherTextJson);
-    $("div.js-tweet-text-container > p").text(parsed.ct+"҂"+parsed.salt+"؏"+parsed.iv+"௳"); // Displays cipher text in tweet
-    // console.log(parsed);
+        sjcl.misc.cachedPbkdf2(group.key, parameters);
+        cipherTextJson = sjcl.encrypt(group.key, text, parameters, rp);
 
+        var parsed = JSON.parse(cipherTextJson);
+        // console.log(cipherTextJson);
+        $(this).text(parsed.ct + "҂" + parsed.salt + "؏" + parsed.iv + "௳"); // Displays cipher text in tweet
+    });
 }
